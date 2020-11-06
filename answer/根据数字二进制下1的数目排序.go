6b5034524f0924b9c11757429d64980af763bc62
Linuxer003 <1127003038@qq.com>
2020-11-06 14:19:40 +0800

    11.6

---
+++ answer/根据数字二进制下1的数目排序.go
@@ -0,0 +1,22 @@
+package answer
+
+import "sort"
+
+var Bit = [1e4 + 1]int{}
+
+// init 采用位运算+递推的方式 计算二进制中1的个数
+func init() {
+	for i := 1; i <= 1e4+1; i++ {
+		//当前数字的含1数量 = 比目标少一位的含1数量 + 当前数字的最后一位与1与运算的结果
+		Bit[i] = Bit[i>>1] + i&1
+	}
+}
+
+func sortByBits(arr []int) []int {
+	sort.Slice(arr, func(i, j int) bool {
+		x, y := arr[i], arr[j]
+		cx, cy := Bit[x], Bit[y]
+		return cx < cy || cx == cy && x < y
+	})
+	return arr
+}
--- main.go
+++ main.go
@@ -1,9 +1,4 @@
 package main
 
-import "leetcode/answer"
-
 func main() {
-	arr := []int{1, 15, 7, 9, 2, 5, 10}
-	k := 3
-	answer.MaxSumAfterPartitioning(arr, k)
 }
