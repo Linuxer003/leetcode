ad14b0cd5551593059556d46d7c494e227505ab6
Linuxer003 <1127003038@qq.com>
2020-11-15 13:57:44 +0800

    11.15

--- answer/移掉k位数字.go
+++ answer/移掉k位数字.go
@@ -2,8 +2,8 @@ package answer
 
 import "strings"
 
-func removeKdigits(num string, k int) string {
-	stack := []byte{}
+func removeKDigits(num string, k int) string {
+	stack := []byte(nil)
 	for i := range num {
 		digit := num[i]
 		for k > 0 && len(stack) > 0 && digit < stack[len(stack)-1] {
@@ -12,6 +12,7 @@ func removeKdigits(num string, k int) string {
 		}
 		stack = append(stack, digit)
 	}
+	// 此时已经删除了部分数字，k随着删除过程减小，但k未必减到0，所以还需要再删除一些。
 	stack = stack[:len(stack)-k]
 	ans := strings.TrimLeft(string(stack), "0")
 	if ans == "" {
