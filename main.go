4e79d48026ca730898e2dae58208b20023af3a31
wgh <1127003038@qq.com>
2020-12-16 00:05:53 +0800

    12.15 final

--- main.go
+++ main.go
@@ -1,11 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"leetcode/answer"
-)
-
 func main() {
-	dis := [][]int{{1, 2}, {3, 4}, {5, 6}, {6, 7}, {8, 9}, {7, 8}}
-	fmt.Println(answer.PossibleBipartition(10, dis))
 }
