5482af5c13add163a9f277da4e5ee353b7250c60
wgh <1127003038@qq.com>
2021-01-12 23:56:45 +0800

    12.15 final

--- main.go
+++ main.go
@@ -7,44 +7,71 @@ import (
 func main() {
 	fmt.Println()
 }
-func maxProfit(k int, prices []int) int {
-	if len(prices) == 0 {
-		return 0
-	}
-	n := len(prices)
 
-	if k >= n/2 {
-		dp0, dp1 := 0, -prices[0]
-		for i := 0; i < n; i++ {
-			tmp := dp0
-			if dp0 < dp1+prices[i] {
-				dp0 = dp1 + prices[i]
-			}
-			if dp1 < tmp-prices[i] {
-				dp1 = tmp - prices[i]
+func topSort(graph [][]int, deg, items []int) (orders []int) {
+	q := []int{}
+	for _, i := range items {
+		if deg[i] == 0 {
+			q = append(q, i)
+		}
+	}
+	for len(q) > 0 {
+		from := q[0]
+		q = q[1:]
+		orders = append(orders, from)
+		for _, to := range graph[from] {
+			deg[to]--
+			if deg[to] == 0 {
+				q = append(q, to)
 			}
 		}
-		if dp0 < dp1 {
-			return dp1
-		} else {
-			return dp0
+	}
+	return
+}
+
+func sortItems(n, m int, group []int, beforeItems [][]int) (ans []int) {
+	groupItems := make([][]int, m+n)
+	for i := range group {
+		if group[i] == -1 {
+			group[i] = m + i
+		}
+		groupItems[group[i]] = append(groupItems[group[i]], i)
+	}
+
+	groupGraph := make([][]int, m+n)
+	groupDegree := make([]int, m+n)
+	itemGraph := make([][]int, n)
+	itemDegree := make([]int, n)
+	for cur, items := range beforeItems {
+		curGroupID := group[cur]
+		for _, pre := range items {
+			preGroupID := group[pre]
+			if preGroupID != curGroupID {
+				groupGraph[preGroupID] = append(groupGraph[preGroupID], curGroupID)
+				groupDegree[curGroupID]++
+			} else {
+				itemGraph[pre] = append(itemGraph[pre], cur)
+				itemDegree[cur]++
+			}
 		}
 	}
 
-	dp := make([][2]int, k+1)
-	for i := 0; i <= k; i++ {
-		dp[i][0] = 0
-		dp[i][1] = -prices[0]
+	items := make([]int, m+n)
+	for i := range items {
+		items[i] = i
 	}
-	for i := 1; i < n; i++ {
-		for j := k; j > 0; j-- {
-			if dp[j-1][1] < dp[j-1][0]-prices[i] {
-				dp[j-1][1] = dp[j-1][0] - prices[i]
-			}
-			if dp[j][0] < dp[j-1][1]+prices[i] {
-				dp[j][0] = dp[j-1][1] + prices[i]
-			}
+	groupOrders := topSort(groupGraph, groupDegree, items)
+	if len(groupOrders) < len(items) {
+		return nil
+	}
+
+	for _, groupID := range groupOrders {
+		items := groupItems[groupID]
+		orders := topSort(itemGraph, itemDegree, items)
+		if len(orders) < len(items) {
+			return nil
 		}
+		ans = append(ans, orders...)
 	}
-	return dp[k][0]
+	return
 }
